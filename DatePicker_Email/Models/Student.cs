1661ffa9a89f67a1ab354cb013209678d0ca80a4
Pavlo Marchuk <pavlo_marchuk@hotmail.com>
2018-01-22 11:18:03 +0200

    Змінено datepicker на чистий bootstrap

--- DatePicker_Email/Models/Student.cs
+++ DatePicker_Email/Models/Student.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatePicker_Email.Models
 {
-    public class Student
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime Birthday { get; set; }
+	public class Student
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		[Required]
+		[Display(Name ="День народження")]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+		[DataType(DataType.Date)]
+		public DateTime Birthday { get; set; }
     }
 }
